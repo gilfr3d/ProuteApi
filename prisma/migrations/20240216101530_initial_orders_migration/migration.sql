f6d1d54b57727a5f005ad990d8da4c36ac69b551
gilfr3d <gil@bsitco.com>
2024-02-16 14:16:18 +0400

    .

--- prisma/migrations/20240216101530_initial_orders_migration/migration.sql
+++ prisma/migrations/20240216101530_initial_orders_migration/migration.sql
@@ -69,6 +69,27 @@ CREATE TABLE [dbo].[Products] (
     CONSTRAINT [Products_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
+-- CreateTable
+CREATE TABLE [dbo].[Orders] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [order_number] BIGINT NOT NULL,
+    [product_name] NVARCHAR(1000) NOT NULL,
+    [gtin] NVARCHAR(1000) NOT NULL,
+    [gln] NVARCHAR(1000) NOT NULL,
+    [customer_name] NVARCHAR(1000) NOT NULL,
+    [customer_email] NVARCHAR(1000) NOT NULL,
+    [shipping_address] NVARCHAR(1000),
+    [state] NVARCHAR(1000),
+    [city] NVARCHAR(1000),
+    [postal_code] NVARCHAR(1000),
+    [order_date] DATETIME2 NOT NULL,
+    [order_total] FLOAT(53) NOT NULL,
+    [mobile_number] NVARCHAR(1000) NOT NULL,
+    [payment_method] NVARCHAR(1000) NOT NULL,
+    CONSTRAINT [Orders_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Orders_customer_email_key] UNIQUE NONCLUSTERED ([customer_email])
+);
+
 -- AddForeignKey
 ALTER TABLE [dbo].[RefreshToken] ADD CONSTRAINT [RefreshToken_userId_fkey] FOREIGN KEY ([userId]) REFERENCES [dbo].[Users]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
